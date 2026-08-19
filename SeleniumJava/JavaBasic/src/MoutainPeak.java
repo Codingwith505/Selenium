39343b4ec7dd0f80991e04c2c37e04343d230e7e
Codingwith505 <sushilmaurya9192@gmail.com>
2026-08-19 19:56:35 +0530

    moutain peak question solve where array is giving in acending and discesnding order

---
+++ SeleniumJava/JavaBasic/src/MoutainPeak.java
@@ -0,0 +1,30 @@
+public class MoutainPeak {
+
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,3,5,4,3,2,1};
+        System.out.println(searchHighestNumber(arr));
+        
+    }
+
+    static int searchHighestNumber(int[] arr){
+
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<=end){
+            int mid = start+(end-start)/2;
+
+            if(arr[mid]<arr[mid+1]){
+                //increasing the moutain towards the peak
+                start = mid+1;
+            }
+            else if(arr[mid]>arr[mid+1]){
+                //decreasing the mautain towards the peak
+                end = mid-1;
+            }
+            
+        }
+        return start;
+    }
+    
+}
