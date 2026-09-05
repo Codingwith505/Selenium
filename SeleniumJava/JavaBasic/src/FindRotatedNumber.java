4244dfe727120123b0612b523fe26532a9e13d06
Codingwith505 <sushilmaurya9192@gmail.com>
2026-09-05 23:43:14 +0530

    add comment to findPivot method for clarity

--- SeleniumJava/JavaBasic/src/FindRotatedNumber.java
+++ SeleniumJava/JavaBasic/src/FindRotatedNumber.java
@@ -3,7 +3,10 @@ public class FindRotatedNumber {
     public static void main(String[] args) {
         
     }
-     static int findPivot(int[] arr){
+
+    //Find largest number in the sorted array
+
+    static int findPivot(int[] arr){
 
         return 0;
     }
