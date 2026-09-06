e660cc53a2c90f7644108a43bdb9bc14bac872dc
Codingwith505 <sushilmaurya9192@gmail.com>
2026-09-06 16:28:13 +0530

    implement findPivot method to determine the pivot index in a rotated sorted array

--- SeleniumJava/JavaBasic/src/FindRotatedNumber.java
+++ SeleniumJava/JavaBasic/src/FindRotatedNumber.java
@@ -1,14 +1,44 @@
 public class FindRotatedNumber {
 
     public static void main(String[] args) {
-        
+
+        int[] arr = {9,0,2,3,5,7};
+
+        System.out.println(numberOfRotation(arr));
+  
+    }
+
+    static int numberOfRotation(int[] arr){
+        int peak = findPivot(arr);
+        return peak+1;
     }
 
     //Find largest number in the sorted array
 
     static int findPivot(int[] arr){
 
-        return 0;
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start<end) { 
+            int mid = start+(end-start)/2;
+
+            // case 1 if mid is greater than next to mid then return pivot 
+
+            if(arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if (arr[start]<arr[mid]){
+                start = mid+1;
+            }
+            else {
+                end = mid;
+            }
+  
+        }
+        return -1;
+
+        
     }
     
 }
