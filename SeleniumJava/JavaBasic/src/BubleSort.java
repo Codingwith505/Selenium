dc71bdf4edeca46341b2788f713f7b7ea2b7cfae
Codingwith505 <sushilmaurya9192@gmail.com>
2026-09-09 23:54:29 +0530

    update BubleSort class: modify main method array and enhance sort method for clarity

--- SeleniumJava/JavaBasic/src/BubleSort.java
+++ SeleniumJava/JavaBasic/src/BubleSort.java
@@ -3,24 +3,30 @@ import java.util.Arrays;
 public class BubleSort {
 
     public static void main (String[] args){
-        int[] arr = {3,2,5,4,1};
+        int[] arr = {4,5,6,7,2,4,7};
         sort(arr);
         System.out.print(Arrays.toString(arr));
 
     }
 
     static void sort(int[] arr){
-
+        boolean swapped;
         for(int i = 0; i<arr.length; i++){
+            swapped = false;
             for(int j =1; j<arr.length-i; j++){
                 if (arr[j]<arr[j-1]) {
                     int temp = arr[j-1];
                     arr[j-1]=arr[j];
                     arr[j]=temp;
+                    swapped = true;
 
                 }
             }
+            if(!swapped){
+                break;
+            }
         }
+
 
     }
     
