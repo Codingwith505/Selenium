73a73ddc41276f402dd6c3d1f7cb978afccfa3c8
Codingwith505 <sushilmaurya9192@gmail.com>
2026-09-10 20:37:30 +0530

    add InsertionSort class with main and insertionSort methods

---
+++ SeleniumJava/JavaBasic/src/InsertionSort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void main (String[] args){
+        int [] arr = {3,5,2,1,4};
+        insertionSort(arr);
+        System.out.print(Arrays.toString(arr));
+
+    }
+    static void insertionSort(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if(arr[j]<arr[j-1]){
+                    swap(arr,j-1,j);
+                }
+                else{
+                    break;
+                }
+                
+            }
+        
+        
+            
+        }
+    }
+
+    static void swap(int[] arr , int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+    
+}
