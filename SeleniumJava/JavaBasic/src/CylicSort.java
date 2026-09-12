72bccf26b221a35b59fb95c5de43921624dcddf2
Codingwith505 <sushilmaurya9192@gmail.com>
2026-09-12 19:11:03 +0530

    add CylicSort class with sorting logic and main method

--- SeleniumJava/JavaBasic/src/CylicSort.java
+++ SeleniumJava/JavaBasic/src/CylicSort.java
@@ -1,3 +1,35 @@
+import java.util.Arrays;
+
 public class CylicSort {
+
+    public static void main (String[] args){
+
+        int[] arr = {1,2,3,6,4,8,7,5};
+        cylicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cylicSort(int[] arr){
+
+        int i = 0;
+        while (i<arr.length-1) { 
+            
+            int correct = arr[i]-1;
+
+            if(arr[i]!= arr[correct]){
+                swapped(arr, i,correct);
+            }
+            else{
+                i++;
+            }
+        }
+
+    }
+
+    static void swapped(int[] arr, int start, int second){
+        int temp = arr[start];
+        arr[start] = arr[second];
+        arr[second] = temp;
+    }
     
 }
